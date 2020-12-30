7ce7e587e034a1f098ad94ce49a6caa39ae86db9
pierre@debian <>
2020-12-30 23:46:02 +0100

    explication unwrap parse

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,7 @@ fn main() {
         return;
     }
     // variables constant during a game
+    // le premier unwrap est nécessaire car opt_str renvoie un Optional. Il ne risque pas d'échouer étant donné que la présence de l'argument est testée par le ternaire. Le second unwrap utilise une valeur par défaut en cas d'erreur de conversion String → u32 par parse(). Le else du ternaire permet d'affecter une valeur par défaut lorsque l'argument n'est pas renseigné.
     let n_players: u32 = if matches.opt_present("p") {matches.opt_str("p").unwrap().parse().unwrap_or(10)} else {10};
     let start_cash: u32 = if matches.opt_present("c") {matches.opt_str("c").unwrap().parse().unwrap_or(5000)} else {5000};
     let first_blind: u32 = if matches.opt_present("b") {matches.opt_str("b").unwrap().parse().unwrap_or(10)} else {10};
@@ -45,4 +46,5 @@ fn main() {
     // auxiliary variables
     let (player_n, n_min_players): (u32, u32);
 
+    
 }
